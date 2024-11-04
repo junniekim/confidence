d80b281d937c88f9799c2cfd66975fe57a36260a
Sijun Kim <junniekim12@gmail.com>
2024-11-03 20:00:17 -0500

    Temporary Push

--- src/progressPage/progressPage.tsx
+++ src/progressPage/progressPage.tsx
@@ -22,29 +22,32 @@ export const bodyWeightOptions = {
   },
   pointSize: 5,
 };
-// export const weightLiftingOptions = {
-//   title: "Max weight change",
-//   curveType: "function",
-//   legend: "none",
-//   vAxis: {
-//     title: "Body Weight (Ibs)",
-//   },
-//   titleTextStyle: {
-//     fontSize: 18,
-//   },
-//   hAxis: {
-//     title: "Day",
-//   },
-//   pointSize: 5,
-// };
+export const weightLiftingOptions = {
+  title: "Max weight change",
+  curveType: "function",
+  legend: "none",
+  vAxis: {
+    title: "Weight (Ibs)",
+  },
+  titleTextStyle: {
+    fontSize: 18,
+  },
+  hAxis: {
+    title: "Day",
+  },
+  pointSize: 5,
+};
 
 //soon as page loads, I need a list of body weight with date and number
 //Also need a list of workout, date, weight
 
 const ProgressPage = () => {
   const [userProgress, setUserProgress] = useState<any>(null);
+  const [chosenWorkout, setChosenWorkout] = useState("");
   const [bodyWeightData, setBodyWeightData] = useState<any>(null);
-  // const [weightLiftingData, setWeightLiftingData] = useState<any>(null);
+  const [weightLiftingProgress, setWeightLiftingProgress] = useState<any>(null);
+  const [weightLiftingData, setWeightLiftingData] = useState<any>(null);
+  const [dropdownOptions, setDropdownOptions] = useState<any>(null);
   const [selectedDate, setSelectedDate] = useState<String>(
     new Date().toLocaleString("en-US", { month: "long", year: "numeric" })
   );
@@ -53,6 +56,8 @@ const ProgressPage = () => {
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
     const currentUser = user || (storedUser ? JSON.parse(storedUser) : null);
+
+    //Query to get user's body weight history
     const query = `${base_url}/user/data/analyze/${currentUser.id}`;
     fetch(query)
       .then((response) => {
@@ -73,15 +78,37 @@ const ProgressPage = () => {
           "error"
         );
       });
+
+    // Query to get users entire workout history
+    const query2 = `${base_url}/log/data/joined/${currentUser.id}`;
+    fetch(query2)
+      .then((response) => {
+        if (!response.ok) {
+          return response.json().then((error) => {
+            throw error.error;
+          });
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setWeightLiftingProgress(data.data);
+        setDropdownOptions(
+          data.data.filter(
+            (workout: any, index: any, self: any) =>
+              index ===
+              self.findIndex((w: any) => w.workout._id === workout.workout._id)
+          )
+        );
+      });
   }, []);
 
   const onChange = (date: any) => {
+    console.log("Date", date);
     let bodyGraphInput: any[] = [];
     setSelectedDate(
       //03-01-2024
       date.toLocaleString("en-US", { month: "long", year: "numeric" })
     );
-    console.log(userProgress);
     userProgress.filter((element: any) => {
       if (
         new Date(element.recordedOn)
@@ -112,12 +139,42 @@ const ProgressPage = () => {
         ]);
       }
     });
-    console.log(bodyGraphInput);
     if (bodyGraphInput.length === 0) {
       setBodyWeightData(null);
     } else {
       bodyGraphInput.unshift(["Day", "Body Weight (Ibs)"]);
       setBodyWeightData(bodyGraphInput);
+    }
+
+    let weightLiftingGraphInput: any[] = [];
+    weightLiftingProgress?.filter((element: any) => {
+      if (
+        element.workout._id == chosenWorkout &&
+        new Date(element.recordedOn)
+          .toLocaleString("en-US", { month: "long", year: "numeric" })
+          .replace(/\//g, "-") === selectedDate
+      ) {
+        weightLiftingGraphInput.push([
+          Number(
+            new Date(element.recordedOn).toLocaleString("en-US", {
+              day: "numeric",
+            })
+          ) + 1,
+          element.weight,
+        ]);
+        weightLiftingGraphInput.sort((a, b) => a[0] - b[0]);
+        weightLiftingGraphInput = weightLiftingGraphInput.map((subArray) => [
+          String(subArray[0]),
+          subArray[1],
+        ]);
+      }
+    });
+    console.log("Graph input", weightLiftingGraphInput);
+    if (weightLiftingGraphInput.length === 0) {
+      setWeightLiftingData(null);
+    } else {
+      weightLiftingGraphInput.unshift(["Day", "Body Weight (Ibs)"]);
+      setWeightLiftingData(weightLiftingGraphInput);
     }
   };
 
@@ -159,6 +216,39 @@ const ProgressPage = () => {
       ) : (
         <h5 className="mt-3 text-center">No Body Weight Recorded</h5>
       )}
+      <hr></hr>
+      <h4 className="text-center mt-4">Your progress in {selectedDate}</h4>
+      {chosenWorkout != null &&
+        (weightLiftingData != null ? (
+          <Chart
+            chartType="LineChart"
+            width="100%"
+            height="400px"
+            loader={<div>Loading Chart</div>}
+            data={weightLiftingData}
+            options={weightLiftingOptions}
+          />
+        ) : (
+          <h5 className="mt-3 text-center">No Weight lifting Recorded</h5>
+        ))}
+      <h5 className="mt-3 text-center">Please choose workout</h5>
+      <select
+        name="workouts"
+        id="workouts"
+        className="form-control"
+        value={chosenWorkout}
+        onChange={(e) => {
+          setChosenWorkout(e.target.value);
+          onChange(selectedDate);
+        }}
+      >
+        <option value="">Select a workout</option>
+        {dropdownOptions?.map((workout: any) => (
+          <option key={Math.random().toFixed(8)} value={workout.workout._id}>
+            {workout.workout.name}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
